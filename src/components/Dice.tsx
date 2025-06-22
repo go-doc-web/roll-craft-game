a8e0c87365ecdaafe4f7462b767aaf2fefb9bd75
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-06-22 22:55:40 +0300

    add images dice

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
+import { motion } from "framer-motion";
 import animationStartDice from "../assets/dice/Property_1_1.svg";
+
 import dice1 from "../assets/dice/Value_1.svg";
 import dice2 from "../assets/dice/Value_2.svg";
 import dice3 from "../assets/dice/Value_3.svg";
@@ -12,10 +14,6 @@ interface DiceProps {
   rolling: boolean;
 }
 
-// type ValueDice = {
-//     [number]:
-// }
-
 const mapDiceImages: Record<number, string> = {
   1: dice1,
   2: dice2,
@@ -27,20 +25,26 @@ const mapDiceImages: Record<number, string> = {
 
 const Dice: FC<DiceProps> = ({ value, rolling }) => {
   return (
-    <div className="w-24 h-24 flex items-center justify-center bg-white rounded shadow-md  ">
+    <div className="w-25 h-25 flex items-center justify-center rounded  ">
       {rolling ? (
-        <img src={animationStartDice} alt="animation" className="w-12 h-12" />
+        <motion.img
+          src={animationStartDice}
+          alt="animation"
+          className="w-20 h-20"
+          animate={{ rotate: 360 }}
+          transition={{ repeat: Infinity, duration: 0.6, ease: "linear" }}
+        />
       ) : value ? (
         <img
           src={mapDiceImages[value]}
           alt={`Dice ${value}`}
-          className="w-12 h-12"
+          className="w-20 h-20"
         />
       ) : (
         <img
           src={mapDiceImages[3]}
           alt={`Dice ${value}`}
-          className="w-12 h-12"
+          className="w-20 h-20"
         />
       )}
     </div>
